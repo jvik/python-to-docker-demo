e8c9d697ce8e358bbcd86eac8db1b34e2a6c4676
jvik <mail@jvik.no>
2026-02-23 13:53:26 +0100

    Update hello_world response message

--- src/python_flask_demo/__init__.py
+++ src/python_flask_demo/__init__.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World from Flask in Docker!'
+    return 'Hello, World from Flask in Docker! Demo!'
 
 @app.route('/health')
 def health():
